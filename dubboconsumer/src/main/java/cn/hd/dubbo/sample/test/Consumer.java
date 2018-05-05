0459382ab39ee2a3141308550d14676bf8ab9ee8
dinghe <dinghe1018@163.com>
2018-05-05 12:10:07 +0800

    1213

--- dubboconsumer/src/main/java/cn/hd/dubbo/sample/test/Consumer.java
+++ dubboconsumer/src/main/java/cn/hd/dubbo/sample/test/Consumer.java
@@ -21,6 +21,7 @@ public class Consumer {
 		System.out.println(hello);
 		
 		List list = sampleService.getUsers();
+
 		if (list != null && list.size() > 0) {
 			for (int i = 0; i < list.size(); i++) {
 				System.out.println(list.get(i));
